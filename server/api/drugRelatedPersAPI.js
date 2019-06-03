679579273d3cc7ee25d0953ab74832321382e36d
shenrui <ilovepumpkin@126.com>
2019-06-03 11:31:50 +0800

    add person fake data

--- server/api/drugRelatedPersAPI.js
+++ server/api/drugRelatedPersAPI.js
@@ -18,45 +18,24 @@ module.exports = class BackendAPI {
     return axios(config).then(res => res.data);
   }
 
+  async getPerData(phoneNumber) {
+    let rawData;
+    if (this.baseUrl) {
+      const url = this.baseUrl + '/drug-related-pers/' + phoneNumber;
+      rawData = await invoke({ url, method: 'get' });
+    } else {
+      rawData = require('./fakedata/person.json');
+    }
+    return rawData;
+  }
+
   async getRelatedPerGraph(phoneNumbers) {
     let rawData;
     if (this.baseUrl) {
       const url = this.baseUrl + '/drug-related-pers/graph';
       rawData = await invoke({ url, method: 'post', data: { phone_nums: phoneNumbers } });
     } else {
-      rawData = {
-        relationships: {
-          phone_num1: ['phone_num2', 'phone_num3', 'phone_num4'],
-          phone_num4: ['phone_num5', 'phone_num6']
-        },
-        'person-data': {
-          phone_num1: {
-            name: '王二1',
-            score: 56.66
-          },
-          phone_num2: {
-            name: '王二2',
-            score: 26.666
-          },
-          phone_num3: {
-            name: '王二3',
-            score: 16.666
-          },
-          phone_num4: {
-            name: '王二4',
-            score: 59.666
-          },
-          phone_num5: {
-            name: '王二5',
-            score: 25.666
-          },
-          phone_num6: {
-            name: '王二6',
-            score: 88.666
-          }
-        },
-        'no-data': ['phone_num7', 'phone_num8']
-      };
+      rawData = require('./fakedata/graph.json');
     }
 
     const result = { 'no-data': rawData['no-data'] };
