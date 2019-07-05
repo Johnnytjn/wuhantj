9bbc68e0efda15c1ca7474d9e99eff7f63c9c56e
shenrui <ilovepumpkin@126.com>
2019-07-05 16:31:52 +0800

    add fake data api

--- fake-api-server/router/index.js
+++ fake-api-server/router/index.js
@@ -1,11 +1,20 @@
 module.exports = function(router) {
   router.post('/drug-related-pers/graph', async ctx => {
-    const { phone_nums } = ctx.request.body;
-    ctx.body = require('./fakedata/graph.json');
+    // const { phone_nums } = ctx.request.body;
+    ctx.body = require('./fakedata/drug/graph.json');
   });
   router.get('/drug-related-pers/:phoneNumber', async ctx => {
-    const { phoneNumber } = ctx.params;
-    const data = require('./fakedata/person.json');
+    // const { phoneNumber } = ctx.params;
+    const data = require('./fakedata/drug/person.json');
+    ctx.body = data;
+  });
+  router.post('/whoring-related-pers/graph', async ctx => {
+    // const { phone_nums } = ctx.request.body;
+    ctx.body = require('./fakedata/whoring/graph.json');
+  });
+  router.get('/whoring-related-pers/:phoneNumber', async ctx => {
+    // const { phoneNumber } = ctx.params;
+    const data = require('./fakedata/whoring/person.json');
     ctx.body = data;
   });
 };
